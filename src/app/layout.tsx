060c7848af43e71ebc882fa8d404e078f46a33df
Yago Militão <37947421+YagoMilitao@users.noreply.github.com>
2025-05-24 11:28:45 -0300

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,23 @@
-import "./globals.css";
-import { ReactNode } from "react";
-import { SessionProvider } from "next-auth/react";
+import './globals.css';
+import { Inter } from 'next/font/google';
+import SessionProviderWrapper from '@/components/SessionProviderWrapper';
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'ANA Gaming App',
+  description: 'Plataforma de visualização de apostas esportivas',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="pt-br">
-      <body>
-        <SessionProvider>{children}</SessionProvider>
+    <html lang="pt-BR">
+      <body className={inter.className}>
+        <SessionProviderWrapper>{children}</SessionProviderWrapper>
       </body>
     </html>
   );
