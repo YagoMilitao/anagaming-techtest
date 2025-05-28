89b258f022b142e3332c974d528771d229fcd6e8
Yago Militão <37947421+YagoMilitao@users.noreply.github.com>
2025-05-28 00:30:34 -0300

    Revert "implement test"
    This reverts commit 1ac228bce6e830f2c1ebbb5b2c681529d36eb334.

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  collectCoverage: true,
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  testMatch: ['**/src/**/*.test.(ts|js)'],
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
-};
